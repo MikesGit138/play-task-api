e7f4fc592d4cef723ccf53b557d34da3cf6d5991
Michael Leighton <michael138leighton@gmail.com>
2023-04-01 22:23:57 -0500

    config completed

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -3,18 +3,20 @@ import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
-
+//import io.ebean.Ebean;
+import io.ebean.DB;
 import model.*;
 import java.util.List;
 import com.google.inject.Inject;
 
 
 public class HomeController extends Controller {
-    
+
+    private final play.db.ebean.EbeanConfig ebeanConfig;
     //constructor
     @Inject
-    public HomeController(){
-
+    public HomeController(play.db.ebean.EbeanConfig ebeanConfig){
+        this.ebeanConfig = ebeanConfig;
     }
 
     public Result index() {
@@ -32,36 +34,59 @@ public class HomeController extends Controller {
     }
 
     //get tasks
-    // public Result getTasks(){
-    //     // return ok(Json.toJson(Task.allTasks()));
-    // }
+     public Result getTasks(){
+         List<Task> tasks = Task.find.all();
+         if (tasks.size() == 0)
+            return ok("You have no tasks as yet");
+         else
+             return ok(Json.toJson(tasks));
+     }
 
     // //create task
-    // public Result createTask(Http.Request request){
-    //     // Task.add(Json.fromJson(request.body().asJson(), Task.class));
-    //     // return ok(Json.toJson(Task.allTasks()));
-    // }
+     public Result createTask(Http.Request request){
+         Task newtask = Json.fromJson(request.body().asJson(), Task.class);
+         if (ebeanConfig == null) {
+             return internalServerError("Ebean configuration is missing");
+         }
+         DB.save(newtask);
+         return ok();
+     }
 
-    // //edit task
-    // public Result editTask(Integer id){
-    //     return ok();
-    // }
+    public Result updateTask(Http.Request request, Integer id) {
+        Task updatedTask = Json.fromJson(request.body().asJson(), Task.class);
+        Task taskToUpdate = DB.find(Task.class, id);
+        if (taskToUpdate != null) {
+            taskToUpdate.setId(id);
+            taskToUpdate.setTitle(updatedTask.getTaskTitle());
+            taskToUpdate.update();
+            return ok("Task of id " + id + " updated");
+        } else {
+            return notFound("Task not found");
+        }
+    }
 
-    // //update task
-    // public Result updateTask(){
-    //     return ok();
-    // }
 
-    // //delete task
-    // public Result deleteTask(Integer id){
-    //     // Task.del(id);
-    //     // return ok(Json.toJson(Task.allTasks()));
-    // }
+    public Result deleteTask(Integer id) {
+        Task taskToDelete = DB.find(Task.class, id);
+        if (taskToDelete != null) {
+            taskToDelete.delete();
+            return ok("Task of id: " + id +" deleted");
+        } else {
+            return notFound("Task if id: " +id + " not found");
+        }
+    }
 
-    
-    // //complete task
-    // public Result completeTask(Integer id){
-    //     // Task.completeTask(id);
-    //     // return ok(Json.toJson(Task.allTasks()));
-    // }
+
+
+    //complete task
+     public Result completeTask(Integer id) {
+         Task taskToComplete = DB.find(Task.class, id);
+         if (taskToComplete != null) {
+             taskToComplete.setIsComplete();
+             taskToComplete.update();
+             return ok("Task of id: " + id + " completed is changed");
+         } else {
+             return notFound("Task of id: " + id + " not found");
+         }
+     }
 }
--- app/model/Task.java
+++ app/model/Task.java
@@ -12,65 +12,29 @@ public class Task extends Model{
     public Boolean completed;
     @Id
     public Integer id;
-   
-    
-    // public Task (Integer id, String taskTitle, Boolean completed){
-    //     this.id = id;
-    //     this.taskTitle = taskTitle;
-    //     this.completed = completed;
-    // }
 
-    //private static Set<Task> tasks;
     public static final Finder<Integer, Task> find = new Finder<>(Task.class);
-    public void setTitle(String title){
-        this.taskTitle = title;
+    //getters
+    public String getTaskTitle(){
+        return taskTitle;
     }
-    public void isComplete(){
-        this.completed = !this.completed;
+    public Integer getId(){
+        return id;
+    }
+    public Boolean getCompleted(){
+        return completed;
+    }
+    //setters
+    public void setTitle(String taskTitle){
+        this.taskTitle = taskTitle;
     }
     public void setId(Integer id){
         this.id = id;
     }
-    public void getTasks(Task task){
-        this.
+    public void setIsComplete(){
+        this.completed = !this.completed;
     }
-    // static {
-    //     tasks = new HashSet<Task>();
-    //     tasks.add(new Task(1, "go running", false));
-    //     tasks.add(new Task(2, "do work", false));
-    //     tasks.add(new Task(3, "do more work", false));
-    //     tasks.add(new Task(4, "go jogging", false));
-    //     tasks.add(new Task(5, "go walking", false));
-    //     tasks.add(new Task(6, "lift weights", false));
-    //     tasks.add(new Task(7, "cook", false));
-    // }
 
-//     public static Set<Task> allTasks(){
-//         return tasks;
-//     }
-//     public void setTitle(String tasks){
-//         this.taskTitle = tasks;
-//    }
-//    public static Task findById(Integer id){
-//         for (Task task: tasks){
-//            if(id.equals(task.id)){
-//                return task;
-//            }
-//         }
-//         return null;
-//    }
-
-//    public static void add(Task task){
-//        tasks.add(task);
-//    }
-
-//    public static void completeTask(Integer id){
-//        findById(id).completed = !findById(id).completed;
-//    }
-
-//    public static void del(Integer id){
-//        tasks.remove(findById(id));
-//    }
 
 }
 
